56111107e553bc030156d6bbdcfde1883ab714a5
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-07-07 00:40:48 +0700

    initial commit

---
+++ DataKaryawan/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataKaryawan
+{
+	public partial class DataKaryawan : Form
+	{
+		public DataKaryawan()
+		{
+			InitializeComponent();
+		}
+
+		private void groupBox2_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void DataKaryawan_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label7_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox6_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
