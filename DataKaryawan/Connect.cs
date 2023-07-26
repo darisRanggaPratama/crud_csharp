58be178546617fe1347ab1036e906a53e1878ad7
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-07-26 21:00:32 +0700

    initial commit

---
+++ DataKaryawan/Connect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataKaryawan
+{
+	internal class Connect
+	{
+		// Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;
+		string connectionstring = "Server=localhost; Database=sample; Uid=rangga; Pwd=rangga;";
+
+
+	}
+}
